2956658dec1516457e533fcf4d64363e346ccc68
cry004 <cry004@gmail.com>
2017-08-14 20:40:52 +0900

    Add project page

--- server.js
+++ server.js
@@ -56,6 +56,12 @@ app.get('/about',(req, res) => {
   });
 })
 
+app.get('/project', (req, res) => {
+  res.render('project.hbs',{
+    pageTitle: 'Project Page'
+  });
+})
+
 app.listen(port, () => {
   console.log(`server is fired on port ${port}`)
 });
